4372c9b4b6e948edcf7a9912df13a2e335454448
User <user@example.com>
2026-01-14 01:16:53 +0500

    feat: add Google AdSense account meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="google-adsense-account" content="ca-pub-8190183432288466" />
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8190183432288466"
